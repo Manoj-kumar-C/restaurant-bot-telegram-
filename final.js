0d420366e0794b120e1dafcc80d6a1489c7585ba
c-manojkumar <developermanoj2003@gmail.com>
2023-04-13 17:33:07 +0530

    Hai hello everyone

--- final.js
+++ final.js
@@ -175,6 +175,36 @@ bot.command("restaurantNa", (ctx)=>{
 
 })
 
+
+
+
+bot.command("foodcourt", (ctx)=>{
+    let inp = ctx.message.text.split(" ")
+
+    if(inp.length != 2){
+        ctx.reply("Must give food name as a second argument .")
+        return;
+    }
+
+    let food = inp[1]
+    axios.get('https://script.googleusercontent.com/macros/echo?user_content_key=dujVW20C-t0KgjP7cPt0QE-CQyE9MC4YAKgoaxBuWCk8pVPTqtcLKIzuiYL5At696S1KpWGrhTSbIPFCa8pZMXwm84LAWjaum5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOAEI1TipRmirKhH_2OaKu0qDzH6QUal99AS0e7OitSp6fN09pjOAx9nznnOq1Yh2ah7gXNb_XGMkl0QSpIj_b7x2nNMOLeTPA&lib=MsOKQEK32Nn_yJD5zTH4WG5GvrR6Rl4Ky')
+     .then(res =>{
+        console.log(res.data.length)
+        for (var i =1; i<4; i++){
+            if(res.data[i].FoodName==food){
+                console.log(res.data[i])
+                ctx.replyWithPhoto(`https://i.ibb.co/gvsDf4P/${food}.jpg`)
+                // res.data[i].Image
+                ctx.reply(`price: ${res.data[i].Price} \n`)
+                ctx.reply("Pay: https://rzp.io/l/l0VZu4mz3")
+            }
+        } 
+     })
+
+    
+
+})
+
 // logger 
 function logger(ctx){
     console.log(ctx.from.username + "said: "+ ctx.message.text)
